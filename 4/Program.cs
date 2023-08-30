233d3a146a81db6871d8042a8c655e91c78cd543
Fennellis <westgeze@gmail.com>
2023-08-30 15:38:51 +0300

    Задача 4 исправлен алгоритм

--- 4/Program.cs
+++ 4/Program.cs
@@ -4,7 +4,7 @@
 // 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
 // 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
 
-int length = 3; int width = 3; int depth = 3;
+int length = 5; int width = 5; int depth = 5;
 
 int [,,] arr = CreateArray(length, width, depth, 10, 99);
 PrintArray(arr);
@@ -16,10 +16,11 @@ int [,,] CreateArray(int columns,int rows, int plane, int min, int max)
         for (int y = 0; y < array.GetLength(1); y++)
             for (int x = 0; x < array.GetLength(2); x++)
             {
-                bool contains = false;
+                bool contains;
                 do
                 {
                     array[z,y,x] = new Random().Next(min, max + 1);
+                    contains = false;
                     for (int i = 0; i < z; i++)
                         for (int j = 0; j < y; j++)
                             for (int k = 0; k < x; k++)
